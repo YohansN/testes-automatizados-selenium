3de5b6a3791050c8b2178f7c229e8988c7899779
Yohans Nascimento <yohans.dev@gmail.com>
2023-10-16 10:33:43 -0300

    added new login test - alternative case : failure in login. Plus refactoring

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -1,21 +1,34 @@
 package login;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.opera.OperaDriver;
 
 public class LoginTest {
+
+    private WebDriver browser;
+    private static final String LOGIN_URL = "http://localhost:8080/login";
+
+    @BeforeAll
+    public static void setUpConfig(){
+        System.setProperty("webdriver.opera.driver", "drivers/operadriver.exe");
+    }
+
+    @BeforeEach
+    public void setUpLoginTest(){
+        this.browser = new OperaDriver();
+        browser.navigate().to(LOGIN_URL);
+    }
+
+    @AfterEach
+    public void quitLoginTest(){
+        this.browser.quit();
+    }
+
     @Test
     public void deveriaEfetuarLoginComDadosValidos(){
-        //Set
-        System.setProperty("webdriver.opera.driver", "drivers/operadriver.exe");
-        WebDriver browser = new OperaDriver();
-        String baseUrl = "http://localhost:8080/login";
-        browser.navigate().to(baseUrl);
-
         //Action
         String username = "fulano";
         String password = "pass";
@@ -24,8 +37,21 @@ public class LoginTest {
         browser.findElement(By.id("login-form")).submit();
 
         //Assert
-        Assert.assertNotEquals(browser.getCurrentUrl(), baseUrl);
+        Assertions.assertNotEquals(browser.getCurrentUrl(), LOGIN_URL);
+    }
 
-        browser.quit();
+    @Test
+    public void naoDeveriaEfetuarLoginComDadosInvalidos(){
+        //Action
+        String username = "fulano";
+        String wrongPassword = "1234";
+        browser.findElement(By.id("username")).sendKeys(username);
+        browser.findElement(By.id("password")).sendKeys(wrongPassword);
+        browser.findElement(By.id("login-form")).submit();
+
+        //Assert
+        Assertions.assertTrue(browser.getCurrentUrl().equals(LOGIN_URL+"?error"));
+        String errorMessage = "Usuário e senha inválidos.";
+        Assertions.assertTrue(browser.getPageSource().contains(errorMessage));
     }
 }
