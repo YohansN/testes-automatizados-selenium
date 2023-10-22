d52e1b7f58f3f96c1c446138b113decc94b3dd98
Yohans Nascimento <yohans.dev@gmail.com>
2023-10-22 12:53:21 -0300

    added more tests in cadastro leiloes

--- src/test/java/leiloes/LeiloesTest.java
+++ src/test/java/leiloes/LeiloesTest.java
@@ -2,17 +2,10 @@ package leiloes;
 
 import driverConfig.DriverConfiguration;
 import login.LoginPage;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class LeiloesTest extends DriverConfiguration {
     private LeiloesPage paginaDeLeiloes;
-
-    @AfterEach
-    public void afterEach(){
-        this.paginaDeLeiloes.fecharPagina();
-    }
 
     @BeforeEach
     public void beforeEach(){
@@ -27,9 +20,110 @@ public class LeiloesTest extends DriverConfiguration {
 
     @Test
     public void deveriaCadastrarLeilao(){
-        this.paginaDeLeiloes.preencheFormularioValido();
-        this.paginaDeLeiloes.submeteFormulario();
+        //setup
+        String nome = "produto 2";
+        Double valor = 200.00;
+        String data = "22/10/2023";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertFalse(!this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isNewLeilao(nome, valor, data));
     }
 
+    @Test
+    public void naoDeveriaCadastrarLeilaoVazio(){
+        //setup
+        String nome = "";
+        Double valor = 1.0;
+        String data = "";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isNomeWarningsOnPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isDataWarningOnPage());
+    }
+
+    @Test
+    public void naoDeveriaCadastrarLeilaoEmBranco(){
+        //setup
+        String nome = " ";
+        Double valor = 0.1;
+        String data = " ";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isNomeWarningsOnPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isDataWarningOnPage());
+    }
+
+    @Test
+    public void naoDeveriaCadastrarLeilaoComNomeMenorQueOMinimo(){
+        //setup
+        String nome = "OI";
+        Double valor = 10.50;
+        String data = "10/10/2023";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isNomeWarningsOnPage());
+    }
+
+    @Test
+    public void naoDeveriaCadastrarLeilaoComValorNegativo(){
+        //setup
+        String nome = "Teste 3";
+        Double valor = -3.0;
+        String data = "10/10/2023";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isValorWarningOnPage());
+    }
+
+    @Test
+    public void naoDeveriaCadastrarLeilaoDataNaoFormatada(){
+        //setup
+        String nome = "Produto 2";
+        Double valor = 12.0;
+        String data = "10102023";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isDataWarningOnPage());
+    }
+
+    @Test
+    public void naoDeveriaCadastrarLeilaoDataImpossivel(){
+        //setup
+        String nome = "Produto 2";
+        Double valor = 12.0;
+        String data = "43/89/2003";
+
+        //Action
+        this.paginaDeLeiloes.cadastrarLeilao(nome, valor, data);
+
+        //Assert
+        Assertions.assertTrue(this.paginaDeLeiloes.isLeiloesPage());
+        Assertions.assertTrue(this.paginaDeLeiloes.isDataWarningOnPage());
+    }
 
 }
